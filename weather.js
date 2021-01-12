09a25a5dcd97b095634016db4116094e8c1d81b4
jaengyo <74586570+jaengyo@users.noreply.github.com>
2021-01-12 13:24:48 +0900

    Create weather

---
+++ weather.js
@@ -0,0 +1,37 @@
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("cant access");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+function init() {
+  loadCoords();
+}
+
+init();
